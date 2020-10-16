c00523a45a67095c5a8c546f4f301bca3fb7aa99
jamesacampbell <james@jamescampbell.us>
2020-10-16 15:28:57 -0400

    Future self, please forgive me and don't hit me with the baseball bat again!

---
+++ evaluator-server/checkarbitrage.py
@@ -0,0 +1,37 @@
+from math import log
+import pandas as pd 
+
+def arbitrage(table):
+    transformed_graph = [[-log(edge) for edge in row] for row in graph]
+
+    # Pick any source vertex -- we can run Bellman-Ford from any vertex and
+    # get the right result
+    source = 0
+    n = len(transformed_graph)
+    min_dist = [float('inf')] * n
+
+    min_dist[source] = 0
+
+    # Relax edges |V - 1| times
+    for i in range(n - 1):
+        for v in range(n):
+            for w in range(n):
+                if min_dist[w] > min_dist[v] + transformed_graph[v][w]:
+                    min_dist[w] = min_dist[v] + transformed_graph[v][w]
+
+    # If we can still relax edges, then we have a negative cycle
+    for v in range(n):
+        for w in range(n):
+            if min_dist[w] > min_dist[v] + transformed_graph[v][w]:
+                return True
+
+    return False
+
+
+def main():
+    df = pd.read_csv('tests/exchangerates.csv')
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
