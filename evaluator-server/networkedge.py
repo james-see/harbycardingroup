bda6ff6f28dab847dcb3c55a60c2bb97ae1166fd
jamesacampbell <james@jamescampbell.us>
2020-10-18 01:11:24 -0400

    bump to 0.0.3-dev:wq

--- evaluator-server/networkedge.py
+++ evaluator-server/networkedge.py
@@ -1,26 +1,44 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import json
+
+with open('testdata/nodes.json') as f:
+    data = json.loads(f.read())
+
+newdata = []
+nodes = []
+for k,y in data.items():
+    fixed = k.split('_')
+    nodes.append(fixed[0])
+    nodes.append(fixed[1])
+    newdata.append((fixed[0], fixed[1], float(y)))
 
 g = nx.DiGraph()
-g.add_node("USD")
-g.add_node("JPY")
-g.add_edge("USD","JPY", weight = 91.9943456)
+# g.add_node("USD")
+# g.add_node("JPY")
+# g.add_edge("USD","JPY", {"weight": 91.9943456})
+# g = nx.cycle_graph(nodes, create_using=nx.DiGraph())
+g.add_nodes_from(nodes)
+g.add_weighted_edges_from(newdata)
 options = {
-    'node_color': 'blue',
-    'node_size': 30,
-    'width': 3,
+    'node_color': 'yellow',
+    'node_size': 330,
+    'width': 1,
     'arrowstyle': '-|>',
-    'arrowsize': 12,
+    'arrowsize': 32,
     'with_labels': True,
-    'font_size': 12
+    'font_size': 10
 }
 # pos = nx.circular_layout(g)
 # nx.draw(g, with_labels=True, node_color='y')
-layout = nx.spring_layout(g)
-nx.draw_networkx_edge_labels(g, layout)
-nx.draw_networkx_nodes(g,layout)
-nx.draw_networkx_edges(g, layout, edge_color='r', arrows = True, arrowstyle = '-|>')
-nx.draw_networkx_labels(g, layout, font_size=12, font_family="sans-serif")
+layout = nx.circular_layout(g)
+nx.draw_networkx_edge_labels(g, layout, label_pos=0.3)
+nx.draw_networkx_nodes(g,layout, node_color='yellow', node_size=500)
+nx.draw_networkx_edges(g, layout, edge_color='r', arrows = True, arrowstyle = '-|>', arrowsize = 20)
+nx.draw_networkx_labels(g, layout, font_size=10, font_family="sans-serif")
 
 # nx.draw_networkx(g, arrows=True, **options)
+
+print(nx.negative_edge_cycle(g))
+print(nx.single_source_bellman_ford_path(g, "USD"))
 plt.show()
